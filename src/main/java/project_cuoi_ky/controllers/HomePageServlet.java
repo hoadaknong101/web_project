1df65c40059147dac5604fc373394409c4752230
hoadaknong101@gmail.com <66413447+hoadaknong101@users.noreply.github.com>
2021-11-17 14:46:23 +0700

    create DAO

--- src/main/java/project_cuoi_ky/controllers/HomePageServlet.java
+++ src/main/java/project_cuoi_ky/controllers/HomePageServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/HomePageServlet")
+@WebServlet("/Home")
 public class HomePageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/project_cuoi_ky/dao/CategoryDAO.java
+++ src/main/java/project_cuoi_ky/dao/CategoryDAO.java
@@ -1,5 +1,10 @@
 package project_cuoi_ky.dao;
 
+import java.util.ArrayList;
+import project_cuoi_ky.beans.*;
+
 public class CategoryDAO {
-	
+	public static ArrayList<Catagory> listCategories(){
+		
+	}
 }
