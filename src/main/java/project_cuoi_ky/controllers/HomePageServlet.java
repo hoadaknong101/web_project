905d3dbccd7da6d7af14d46f22248cbd82ce5088
hoadaknong101@gmail.com <66413447+hoadaknong101@users.noreply.github.com>
2021-11-19 15:10:37 +0700

    Add views and setting

--- src/main/java/project_cuoi_ky/controllers/HomePageServlet.java
+++ src/main/java/project_cuoi_ky/controllers/HomePageServlet.java
@@ -1,23 +1,39 @@
 package project_cuoi_ky.controllers;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Home")
+import project_cuoi_ky.beans.Catagory;
+import project_cuoi_ky.dao.CategoryDAO;
+
+@WebServlet("/")
 public class HomePageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public HomePageServlet() {
 		super();
 	}
-
+ 
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+		ArrayList<Catagory> listCategory = new ArrayList<Catagory>();
+		try {
+			listCategory = CategoryDAO.listCategories();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		request.setAttribute("listCategory", listCategory);
+		request.getRequestDispatcher("WEB-INF/views/index.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
